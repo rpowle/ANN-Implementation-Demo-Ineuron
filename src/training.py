bc6822e74896be19bb709f7812153e6195c03398
rpowle <rajpowle12@gmail.com>
2021-10-09 12:15:27 -0400

    data load

--- src/training.py
+++ src/training.py
@@ -1,17 +1,24 @@
-from src.utils.common import read_config
+import sys
+sys.path.insert(0, '../src')
+from utils.common import read_config
 import argparse
+from utils.data_management import get_data
 
 
 def training(config_path):
-    config=read_config(config_path)
+    config = read_config(config_path)
 
-    print(config)
+    validation_datasize = config["params"]["validation_datasize"]
+    (x_train, y_train), (x_valid, y_valid), (x_test, y_test) = get_data(validation_datasize)
+    
 
 if __name__== '__main__':
     args=argparse.ArgumentParser()
 
-    args.add_argument("--config", "-c", default="config.ymal")
-    parsed_arg=args.parse_args()
+    args.add_argument("--config", "-c", default="config.yaml")
+
+    parsed_args = args.parse_args()
 
 
-    training(config_path=parsed_arg.config)
+    
+    training(config_path = parsed_args.config)
